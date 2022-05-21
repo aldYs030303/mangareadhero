a216ee4a3e90f1d7581012b42c7a4bafe4c44167
Aldiyar <61989249+aldYs030303@users.noreply.github.com>
2022-05-21 11:30:01 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const port = 3000;
+const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+
+app.use(express.static("public"));
+app.set("views engine", "ejs");
+
+
+app.use("/", require("./routers/index"));
+app.use("/features", require("./routers/features"));
+app.use("/post", require("./routers/post"));
+app.use("/contact", require("./routers/contact"));
+app.use("/about", require("./routers/about"));
+app.use("/news", require("./routers/news"));
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`)
+)
+
